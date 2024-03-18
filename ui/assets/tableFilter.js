747b98af3aa56b58a364ae48ccd4d2b4a0dac3a9
Travis Paul <travis.paul@mnx.io>
2024-03-18 14:24:31 -0500

    Add filtering to image import table

---
+++ ui/assets/tableFilter.js
@@ -0,0 +1,40 @@
+import { $, $$ } from './global'
+
+// Client-side table filtering
+document.addEventListener('DOMContentLoaded', () => {
+  const $main = $('#main')
+
+  function filterTable ({ target }) {
+    if (target.classList.contains('filterable')) {
+      const value = target.value.trim().toLowerCase()
+      const $rows = $$(target.dataset.rows)
+      if (value.length === 0) {
+        target.nextElementSibling.classList.add('hidden')
+        $rows.forEach(tr => tr.classList.remove('hidden'))
+        return
+      }
+      target.nextElementSibling.classList.remove('hidden')
+      $rows.forEach(tr => {
+        tr.classList.add('hidden')
+        tr.querySelectorAll('.filter-subject').forEach(s => {
+          if (s.textContent.toLowerCase().includes(value)) {
+            tr.classList.remove('hidden')
+          }
+        })
+      })
+    }
+  }
+
+  $main.addEventListener('keyup', filterTable)
+  $main.addEventListener('change', filterTable)
+  $main.addEventListener('click', ({ target }) => {
+    const $target = target.classList.contains('filter-clear')
+      ? target
+      : target.closest('.filter-clear')
+    if ($target) {
+      const $input = $target.previousElementSibling
+      $input.value = ''
+      filterTable({ target: $input })
+    }
+  })
+})
